4e0d384281176f97669c7fc9fe31a37d07b465dc
Horace Heaven <hheaven@medullan.com>
2016-11-04 11:35:26 -0400

    check ready state endpoint

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var routes = require('./routes/index');
+var routes = require('./routes/mongo');
 var users = require('./routes/users');
 
 var app = express();
---
+++ routes/mongo.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var router = express.Router();
+
+var stateMap = {
+	0: 'disconnected',
+	1: 'connected',
+	2: 'connecting',
+	3: 'disconnecting'
+};
+
+/* GET home page. */
+router.get('/mongostate', function(req, res, next) {
+	res.json({
+		state: stateMap[mongoose.connection.readyState]
+	});
+});
+
+module.exports = router;
