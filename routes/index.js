7d4aae2bf3b89bcc664f71c5e81fb617d1320778
Horace Heaven <hheaven@medullan.com>
2016-11-04 11:46:24 -0400

    clean up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var stateMap = {
+  0: 'disconnected',
+  1: 'connected',
+  2: 'connecting',
+  3: 'disconnecting'
+};
+
+router.get('/mongostate', function(req, res, next) {
+  res.json({
+    state: stateMap[mongoose.connection.readyState]
+  });
 });
 
 module.exports = router;
